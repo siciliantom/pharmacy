6f860714766233377a4211cfcbb1fec6e663fe8e
Kate <museland78@gmail.com>
2016-10-24 10:56:55 +0300

    latest pom and web

--- src/main/java/by/sam/pharmacy/controller/HelloController.java
+++ src/main/java/by/sam/pharmacy/controller/HelloController.java
@@ -1,6 +1,8 @@
 package by.sam.pharmacy.controller;
 
 import by.sam.pharmacy.Performer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
@@ -15,7 +17,10 @@ import java.io.IOException;
  * Created by Kate on 22.10.2016.
  */
 public class HelloController extends HttpServlet {
-
+//    static {
+//        new DOMConfigurator().doConfigure("log4j.xml", LogManager.getLoggerRepository());
+//    }
+    final Logger logger = LoggerFactory.getLogger(HelloController.class);
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //XmlWebApplicationContext ctx = new XmlWebApplicationContext(); //ClassPathXmlApplicationContext("spring-config.xml");
@@ -26,7 +31,9 @@ public class HelloController extends HttpServlet {
 
         Performer performer = (Performer) ctx.getBean("duke");
         request.setAttribute("wow", performer.perform("beans power!!"));
-        RequestDispatcher dispatcher = request.getRequestDispatcher("jsp/show.jsp");
+        System.out.print("not");
+        logger.info("Message: ", request.getAttribute("wow").toString());
+        RequestDispatcher dispatcher = request.getRequestDispatcher("view/show.jsp");
         dispatcher.include(request, response);
     }
 }
--- src/main/java/by/sam/pharmacy/z.java
+++
@@ -1,7 +0,0 @@
-package by.sam.pharmacy;
-
-/**
- * Created by Kate on 22.10.2016.
- */
-public class z {
-}
